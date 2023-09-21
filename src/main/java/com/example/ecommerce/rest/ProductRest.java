9ceeb690d01038e9003bfc4c67b710e96b033c44
koshi <chayakoshala.pvt@gmail.com>
2023-09-21 21:26:20 +0530

    product find by id

--- src/main/java/com/example/ecommerce/rest/ProductRest.java
+++ src/main/java/com/example/ecommerce/rest/ProductRest.java
@@ -2,10 +2,7 @@ package com.example.ecommerce.rest;
 
 import com.example.ecommerce.wrapper.ProductWrapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -21,5 +18,10 @@ public interface ProductRest {
 
     @PostMapping("/update")
     ResponseEntity<String> updateProduct(@RequestBody(required = true) Map<String,String> requestMap);
+
+    @PostMapping(path = "/delete{id}")
+    ResponseEntity<String> deleteProduct(@PathVariable Integer id);
+
+
 
 }
