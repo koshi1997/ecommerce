172658620933bc2dba9c66724c51e1ea534a3076
koshi <chayakoshala.pvt@gmail.com>
2023-09-22 14:52:31 +0530

    bill for Pdf

--- src/main/java/com/example/ecommerce/servicelmpl/BillServiceImpl.java
+++ src/main/java/com/example/ecommerce/servicelmpl/BillServiceImpl.java
@@ -7,15 +7,20 @@ import com.example.ecommerce.dao.BillDao;
 import com.example.ecommerce.service.BillService;
 import com.example.ecommerce.utils.CafeUtils;
 import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+
 import java.io.FileOutputStream;
 import java.util.Map;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
@@ -54,7 +59,26 @@ public class BillServiceImpl implements BillService {
                 setRectangleInPdf(document);
 
                 Paragraph chunk = new Paragraph("Cafe Management System",getFont("Header"));
+                chunk.setAlignment(Element.ALIGN_CENTER);
+                document.add(chunk);
 
+                Paragraph paragraph = new Paragraph(data+"\n \n",getFont("Data"));
+                document.add(paragraph);
+
+                PdfPTable table = new PdfPTable(5);
+                table.setWidthPercentage(100);
+                addTableHeader(table);
+
+                JSONArray jsonArray =CafeUtils.getJsonArrayFromString((String) requestMap.get("productDetails"));
+                for (int i=0; i<jsonArray.length(); i++){
+                    addRow(table,CafeUtils.getMapFromJson(jsonArray.getString(i)));
+                }
+                document.add(table);
+
+                Paragraph footer = new Paragraph("Total :" +requestMap.get("totalAmount")+"\n"+"thank you for visiting.please visit again!!",getFont("Data"));
+                document.add(footer);
+                document.close();
+                return new ResponseEntity<>("{\"uuid\":\""+fileName+"\"}",HttpStatus.OK);
 
             }
             return CafeUtils.getResponseEntity("Required data not found", HttpStatus.BAD_REQUEST);
@@ -62,6 +86,31 @@ public class BillServiceImpl implements BillService {
             ex.printStackTrace();
         }
         return CafeUtils.getResponseEntity(CafeContants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private void addRow(PdfPTable table, Map<String, Object> data) {
+        log.info("Inside addRow");
+        table.addCell((String) data.get("name"));
+        table.addCell((String) data.get("category"));
+        table.addCell((String) data.get("quantity"));
+        table.addCell(Double.toString((Double)data.get("price")));
+        table.addCell(Double.toString((Double)data.get("total")));
+
+    }
+
+    private void addTableHeader(PdfPTable table) {
+        log.info("Inside addTableHeader");
+        Stream.of("Name","Category","Quantity","Price","Sub Total")
+                .forEach(columTitle ->{
+                    PdfPCell header = new PdfPCell();
+                    header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                    header.setBorderWidth(2);
+                    header.setPhrase(new Phrase(columTitle));
+                    header.setBackgroundColor(BaseColor.YELLOW);
+                    header.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    header.setVerticalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(header);
+                });
     }
 
     private Font getFont(String type) {
--- src/main/java/com/example/ecommerce/utils/CafeUtils.java
+++ src/main/java/com/example/ecommerce/utils/CafeUtils.java
@@ -1,9 +1,17 @@
 package com.example.ecommerce.utils;
 
+import com.google.common.base.Strings;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import net.bytebuddy.description.method.MethodDescription;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CafeUtils {
 
@@ -23,4 +31,16 @@ public class CafeUtils {
         return "BILL-" +time;
     }
 
+    public static JSONArray getJsonArrayFromString(String data) throws JSONException{
+        JSONArray jsonArray = new JSONArray(data);
+        return jsonArray;
+    }
+
+    public static Map<String,Object> getMapFromJson(String data){
+       if (!Strings.isNullOrEmpty(data))
+           return new Gson().fromJson(data, new TypeToken<Map<String,Object>>(){
+
+           }.getType());
+       return new HashMap<>();
+    }
 }
